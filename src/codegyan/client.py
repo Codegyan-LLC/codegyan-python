83caadf2c0b8971c1cc8764c5b7e619a06344fd5
Prathmesh Yelne <prathameshyelane@gmail.com>
2026-09-14 15:47:07 +0530

    refactor(client): implement singleton pattern for Codegyan class
    Implement the singleton pattern in the Codegyan class using the `__new__`
    method to ensure only one instance is created per session. Added an
    `_initialized` flag to prevent re-initialization of API clients if the
    singleton instance is requested again.

--- src/codegyan/client.py
+++ src/codegyan/client.py
@@ -2,9 +2,23 @@ from .compiler import CompilerApiClient  # Import the CompilerApiClient class fr
 from .tools import ToolsApiClient  # Import the ToolsApiClient class from .tools module
 
 class Codegyan:
+    # Singleton pattern to ensure only one instance of the class is created
+    _instance = None
+    
+    # Override the __new__ method to ensure only one instance of the class is created
+    def __new__(cls, api_key, client_id):
+        if cls._instance is None:
+            cls._instance = super(Codegyan, cls).__new__(cls)
+            # Initialize only once
+            cls._instance._initialized = False
+        return cls._instance
+    
+    # Initialize the Codegyan API client with API key and client ID
     def __init__(self, api_key, client_id):
-        # Initialize the Codegyan API client with API key and client ID.
-
+        # Prevent re-initialization
+        if self._initialized:
+            return
+            
         # Args:
         # - api_key (str): The API key for authentication.
         # - client_id (str): The client ID for identification.
@@ -14,3 +28,6 @@ class Codegyan:
         # Initialize the instances
         self.compilerApiClient = CompilerApiClient(self.api_key, self.client_id)
         self.toolsApiClient = ToolsApiClient(self.api_key, self.client_id)
+        
+        # Mark as initialized
+        self._initialized = True
